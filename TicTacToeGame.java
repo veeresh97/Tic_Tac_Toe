f358c3f516a7526bf264b03f256d96b521b6db26
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-05 13:53:31 +0530

    Remodified: creating the board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,12 +1,14 @@
 
 public class TicTacToeGame {
 	//creating board
-	 public static void createBoard() {
+	 public static char [] createBoard() {
 	        char[] board = new char[10];
 	        for (int i = 1; i < board.length; i++) {
 	            board[i] = ' ';
 	        }
+	        return board;
 	    }
+	     
 
 	    public static void main(String[] args) {
 	        createBoard();
