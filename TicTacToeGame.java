448656bf13e35fb1667125d3744b3ca3e80feac8
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-05 14:27:49 +0530

    Added: choice X or O for User

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,17 +1,32 @@
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	//creating board
-	 public static char [] createBoard() {
-	        char[] board = new char[10];
-	        for (int i = 1; i < board.length; i++) {
-	            board[i] = ' ';
-	        }
-	        return board;
-	    }
-	     
+ //creating board
+    public static char[] createBoard() {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+ //creating to make choice for X or O
+    public static void makeChoice() {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter your choice X or O  ");
+        char player = obj.next().charAt(0);
+        char computer = ' ';
+        if (player == 'X' || player == 'x') {
+            computer = 'O';
+        } else if (player == 'O' || player == 'o'){
+            computer = 'X';
+        } else {
+            System.out.println("invalid");
+        }
+    }
 
-	    public static void main(String[] args) {
-	        createBoard();
-	    }
-	}
-
+    public static void main(String[] args) {
+        createBoard();
+        makeChoice();
+    }
+}
