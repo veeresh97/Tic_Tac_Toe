5e0f2abb297411ce3854fcadf987d4ca8f1ccfc4
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-06 09:36:15 +0530

    Added: Board is displayed

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,22 +2,22 @@
 import java.util.Scanner;
 
 public class TicTacToeGame {
- //creating board
-    public static char[] createBoard() {
+ 
+    public static char[] createBoard() {                      //creating board
         char[] board = new char[10];
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
         return board;
     }
- //creating to make choice for X or O
-    public static void makeChoice() {
+
+    public static void makeChoice() {                         //creating to make choice for X or O
         Scanner obj = new Scanner(System.in);
-        System.out.println("Enter your choice X or O  ");
+        System.out.println("Enter your choice X or O  ");     //taking user input
         char player = obj.next().charAt(0);
         obj.close();
         char computer = ' ';
-        if (player == 'X' || player == 'x') {
+        if (player == 'X' || player == 'x') {                 //assigning the user choice based on the input
             computer = 'O';
         } else if (player == 'O' || player == 'o'){
             computer = 'X';
@@ -26,8 +26,19 @@ public class TicTacToeGame {
         }
     }
 
+                                                              //displaying the board
+    public static void displayBoard(char[] board){
+        System.out.println(board[1]+" | "+board[2]+" | "+board[3]);
+        System.out.println("---------");
+        System.out.println(board[4]+" | "+board[5]+" | "+board[6]);
+        System.out.println("---------");
+        System.out.println(board[7]+" | "+board[8]+" | "+board[9]);
+        System.out.println(board);
+    }
+    
     public static void main(String[] args) {
-        createBoard();
+        char[] board =createBoard();
         makeChoice();
+        displayBoard(board);
     }
 }
